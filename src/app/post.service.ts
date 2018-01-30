2fb17d4002cbddc0d9e4ca50780666c2c9e5014f
StijnDefour <defourstijn10@gmail.com>
2018-01-30 16:00:34 +0100

    Post observable

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -8,8 +8,17 @@ export class PostService {
 
    constructor(private http: HttpClient) {}
 
+   private postsUrl = "http://jsonplaceholder.typicode.com/posts"
+
    // get all posts
-   getAllPosts(): Observable<Post[]> {}
+   getAllPosts(): Observable<Post[]> {
+     return this.http.get(this.postsUrl)
+                         // ...and calling .json() on the response to return data
+                          .map((res:Response) => res.json())
+                          //...errors if any
+                          .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+
+   }
 
    // get comments based on the index
    getCommentsForPost(index: number): Observable<Comment[]> {}
